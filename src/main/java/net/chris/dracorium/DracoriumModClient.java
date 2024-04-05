2e3e32d677c3bebdd66c80afee3b52b87ca75bd5
Christian Thiel <ct.good.to.kow@gmai.com>
2024-04-05 14:57:45 +0200

    Initial commit

---
+++ src/main/java/net/chris/dracorium/DracoriumModClient.java
@@ -0,0 +1,9 @@
+package net.chris.dracorium;
+
+import net.fabricmc.api.ClientModInitializer;
+public class DracoriumModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chris/dracorium/DracoriumModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chris.dracorium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DracoriumModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
